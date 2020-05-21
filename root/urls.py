e36b1938b3d9691ba3e0bd13f7481b4dd1c411c2
Sameer <itsmdsameerkhan@gmail.com>
2020-05-21 20:18:04 +0545

    100% Merge Conflict

--- root/urls.py
+++ root/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-]
- + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path("musics/", include("musics.urls")),
+    path("movies/", include("movies.urls")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
