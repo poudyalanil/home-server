24f301b50e237669658a6782a763d306cd6f4eed
Sameer <itsmdsameerkhan@gmail.com>
2020-05-21 20:30:06 +0545

    Branching Test

--- movies/views.py
+++ movies/views.py
@@ -3,5 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hey! Movie Herni?")
-
+    return render(request, "movies/index.html")
